823214f96beac7680ed518f8e2e2a751227ee6d4
robdcampbell <rdeancampbell@gmail.com>
2023-04-07 14:06:04 -0400

    Canvas time

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
 //console.log("Hello World");
 
 const canvas = document.querySelector("canvas");
+// 2D context
+const context = canvas.getContext("2d");
+
+// takes 4 params - x,y,width,height
+context.fillStyle = "red";
+context.fillRect(10, 10, 100, 50);
+
+context.beginPath();
+context.rect(50, 50, 100, 50);
+context.stroke();
