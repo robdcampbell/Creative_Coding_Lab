95b761399b903306a5d07967622f41a8d11ad994
robdcampbell <rdeancampbell@gmail.com>
2023-04-08 09:07:42 -0400

    Nesting Squares, looping render

--- main.js
+++ main.js
@@ -5,13 +5,38 @@ const canvas = document.querySelector("canvas");
 const context = canvas.getContext("2d");
 
 // takes 4 params - x,y,width,height
-context.fillStyle = "red";
-context.fillRect(10, 10, 200, 200);
+// context.fillStyle = "red";
+// context.fillRect(10, 10, 200, 200);
 
-context.beginPath();
-context.rect(50, 50, 100, 100);
-context.stroke();
+// context.beginPath();
+// context.rect(100, 100, 200, 200);
+// context.stroke();
 
-context.beginPath();
-context.arc(200, 200, 50, 0, 2 * Math.PI);
-context.stroke();
+// context.beginPath();
+// context.arc(200, 200, 50, 0, 2 * Math.PI);
+// context.stroke();
+
+let years = [2040, 2041, 2042];
+let menu = ["starter", "main", "desert", "drinks"];
+
+for (let i = 0; i < 5; i++) {
+  for (let j = 0; j < 5; j++) {
+    let width = 60;
+    let height = 60;
+    let gap = 20;
+    let x = 100 + (width + gap) * i;
+    let y = 100 + (height + gap) * j;
+
+    context.beginPath();
+    context.rect(x, y, width, height);
+    context.stroke();
+
+    context.beginPath();
+    context.rect(x + 8, y + 8, width - 16, height - 16);
+    context.stroke();
+
+    // context.beginPath();
+    // context.arc(x + 30, y + 30, 20, 0, 2 * Math.PI);
+    // context.stroke();
+  }
+}
