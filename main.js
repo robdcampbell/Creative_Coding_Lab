73fa017d59743ccbef3f83aead8900a51430ee57
robdcampbell <rdeancampbell@gmail.com>
2023-04-07 22:10:31 -0400

    Makin Shapes

--- main.js
+++ main.js
@@ -6,8 +6,12 @@ const context = canvas.getContext("2d");
 
 // takes 4 params - x,y,width,height
 context.fillStyle = "red";
-context.fillRect(10, 10, 100, 50);
+context.fillRect(10, 10, 200, 200);
 
 context.beginPath();
-context.rect(50, 50, 100, 50);
+context.rect(50, 50, 100, 100);
+context.stroke();
+
+context.beginPath();
+context.arc(200, 200, 50, 0, 2 * Math.PI);
 context.stroke();
